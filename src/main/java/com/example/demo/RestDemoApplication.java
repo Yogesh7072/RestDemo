6466260068a5045370ab50ce59cdf1c926b5cb20
yogesh7072 <yogeshwarchate7072@gmail.com>
2023-11-08 14:50:27 +0530

    this is no commit

--- src/main/java/com/example/demo/RestDemoApplication.java
+++ src/main/java/com/example/demo/RestDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestDemoApplication {
 
+	// demo
 	public static void main(String[] args) {
 		SpringApplication.run(RestDemoApplication.class, args);
 	}
