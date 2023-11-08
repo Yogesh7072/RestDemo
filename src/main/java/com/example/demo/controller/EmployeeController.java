18fba9a8c21cbd16dbfb3d6e73f6040bbdc32cfc
yogesh7072 <yogeshwarchate7072@gmail.com>
2023-11-08 14:18:01 +0530

    this is first commit

---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employeecont")
+public class EmployeeController {
+	@GetMapping("/getstudentdata")
+	public String getdata() {
+		return "this is employee data";
+	}
+	
+	
+	@GetMapping(value =  "/getemp" , produces = "application/json")
+	public String getdatas() {
+		return "this is employee data";
+	}
+
+}
